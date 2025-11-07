01cb2ae5184d50d7d6ce7c668303d1db0d666c8e
atacadaoqueirozat-rgb <atacadaoqueirozat@gmail.com>
2025-11-07 08:52:51 -0300

    Update and rename VAR.JS to valida.js
    reformulação para supervisores

---
+++ valida.js
@@ -0,0 +1,30 @@
+const usuarios = [ 
+   
+  //supervisores
+{
+und:'41',
+login:'luis',
+pass: '2017',
+linkd:'https://atacadaoqueirozat-rgb.github.io/geral_vendedores/',
+linkdb:'',
+linkdc:''
+},
+
+
+{
+und:'41',
+login:'KAYSE',
+pass: '54321',
+linkd:'https://docs.google.com/spreadsheets/d/1YMyddSOgJX1hRgKbnmttfFwTuTO78mxJla0ANSv0reM/edit?usp=sharing',
+linkdb:'',
+linkdc:'https://drive.google.com/drive/folders/1nTfGJM4izNbROAGVx4LVM3rz0SwJG1Dl?usp=sharing'
+},
+{
+und:'41',
+login:'GARI',
+pass: '852741',
+linkd:'',
+linkdb:'',
+linkdc:''
+},
+]
